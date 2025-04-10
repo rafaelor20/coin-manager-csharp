bffe95b923d2e1ad0c976666d0dae0066622fbcb
Rafael Oliveira Rosário <rafaelor20@hotmail.com>
2025-04-10 10:05:52 -0300

    Update AssemblyInfo for versioning consistency

--- back-end/obj/Debug/net9.0/back-end.AssemblyInfo.cs
+++ back-end/obj/Debug/net9.0/back-end.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("back-end")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fdb67774453a40acd47f689b2fd54b916b923028")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1f2f2ca2f7388e46331e7072ab55f1da55262465")]
 [assembly: System.Reflection.AssemblyProductAttribute("back-end")]
 [assembly: System.Reflection.AssemblyTitleAttribute("back-end")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
